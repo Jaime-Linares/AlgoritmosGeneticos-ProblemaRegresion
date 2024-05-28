687aa2c3df4b3b2e722e26355786e37155e3aefa
Jaime-Linares <jailinbar@alum.us.es>
2024-05-28 20:58:51 +0200

    [feat] fitness function

--- src/AG_LinaresBarrera_ChicoCastellano.py
+++ src/AG_LinaresBarrera_ChicoCastellano.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
+import cmath
 
 from src.Poblacion import Poblacion
 
 
 class AG:
 
-    # Contructor
+    # contructor
     def __init__(self, datos_train, datos_test, seed, nInd, maxIter):
         self.datos_train = datos_train
         self.datos_test = datos_test
@@ -15,22 +16,100 @@ class AG:
         self.maxIter = maxIter
 
 
-    # Función para ejecutar el AG
+
+    # función para ejecutar el AG
     def run(self):
-        np.random.seed(self.seed)
+        np.random.seed(self.seed)       # semilla para reproducibilidad
 
-        datos = pd.read_csv(self.datos_train) 
+        # --------------------------------------------------------------------------------------
+        # ------CARGAMOS LOS DATOS DE ENTRENAMIENTO Y OBTENEMOS EL MEJOR CROMOSOMA POSIBLE------
+        datos = pd.read_csv(self.datos_train, na_values=0)     
 
-        # Inicializar población
+        # inicializar población
         poblacion = Poblacion(self.nInd, datos.shape[1]-1)   # nAtributos = nColumnas - 1
         poblacion_inicial = poblacion.initial()
 
-        
+        # fitness de la población inicial
+        fitness_poblacion_inicial = self.__fitness_poblacion(datos, poblacion_inicial)
+        print(fitness_poblacion_inicial)
 
 
+        # --------------------------------------------------------------------------------------
+        # ------------------------------MEJOR SOLUCIÓN ENCONTRADA-------------------------------
+        ind = [0.5, -0.3, 0.1] 
 
-        # SOLUCIÓN TEMPORAL
-        ind = [0.5, -0.3, 0.1]  
+
+        # --------------------------------------------------------------------------------------
+        # --------------------------SOLUCIÓN SOBRE EL CONJUNTO DE TEST--------------------------
         y_pred = [1.5, 2.3, -0.9]  
+
+
+        # --------------------------------------------------------------------------------------
+        # devolvemos la mejor solución encontrada y las predicciones sobre el conjunto de test
         return ind, y_pred
+    
+
+
+    # función para calcular el fitness de la población
+    def __fitness_poblacion(self, datos, poblacion):
+        matrix_errors = np.zeros((datos.shape[0], poblacion.shape[0]))   # matriz de ceros de dimension nDatos x nInd
+
+        # obtenemos para cada ecuacion una lista con el error (diferencia al cuadrado) de cada uno de
+        # los individuos de la población
+        for i in range(0, datos.shape[0]):
+            for j in range(0, poblacion.shape[0]):
+                matrix_errors[i, j] = self.__evalua_individuo(datos.iloc[i], poblacion[j, :])
+        
+        # calculamos el fitness de cada individuo como la suma de sus errores en cada una de las ecuaciones
+        res = np.zeros(self.nInd)
+        for i in range(0, self.nInd):
+            res[i] = np.sum(matrix_errors[:, i])   # sumamos los errores de cada ecuacion para el individuo i
+
+        return res
+
+
+
+    # función para calcular la diferenca entre el valor real y el valor obtenido para tal individuo
+    # en tal ecuacion
+    def __evalua_individuo(self, valores, parametros):
+        res = 0
+        calculos_prohibidos = False     # para evitar divisiones por 0, 0 elevado a 0 o raices cuadradas de negativos
+
+        y_real = valores.iloc[-1]
+        y_aprox = 0
+        
+        # calculamos la y_aprox (la ecuacion) para el individuo
+        for i in range(0, (parametros.size)-1, 2):    # primero calculamos la suma de los terminos 
+            x_i = i // 2        # división entera
+            par_i = i
+
+            if valores.iloc[x_i] == 0 and parametros[par_i+1] < 0:  # division por 0
+                y_aprox += 0
+                calculos_prohibidos = True
+                break
+            elif valores.iloc[x_i] == 0 and parametros[par_i+1] == 0: # 0 elevado a 0
+                y_aprox += 0
+                calculos_prohibidos = True
+                break
+            else:
+                potencia = valores.iloc[x_i] ** parametros[par_i+1]
+                if np.isnan(potencia):              # numero imaginario
+                    y_aprox += 0
+                    calculos_prohibidos = True
+                    break
+                else:
+                    suma = parametros[par_i] * potencia
+                    if(suma > 1000000000000 and y_aprox > 1000000000000):   # para evitar valores muy grandes
+                        y_aprox += 0
+                        calculos_prohibidos = True
+                        break
+                    else:
+                        y_aprox += parametros[par_i] * potencia
+        y_aprox += parametros[-1]   # sumamos el término independiente
+
+        if calculos_prohibidos:
+            res = 1000000000000
+        else:
+            res = (y_real - y_aprox) ** 2   # calculamos el cuadrado de la diferencia
+        return res
 
