1f80b657f892779aa0db5c32e8552d6bd566b7c6
Jaime-Linares <jailinbar@alum.us.es>
2024-05-29 14:39:22 +0200

    [fix] crossing corrected

--- src/Cruce.py
+++ src/Cruce.py
@@ -2,49 +2,48 @@ import numpy as np
 import random
 import math
 
+
 class Cruce:
 
-    def __init__(self, padres,numero_individuos):
+    def __init__(self, padres, numero_individuos, probabilidad_no_cruce):
         self.padres = padres
         self.numero_individuos= numero_individuos
+        self.probabilidad_no_cruce = probabilidad_no_cruce
            
     
-    def cruce(self):
+    def cruzar(self):
         hijos = np.empty_like(self.padres)
         
-        #Primero seleccionamos los individuos que van a pasar sin cruces, es decir, pasan tal cual:
-        #Seleccionamos el 20% aleatoriamente
-        numero= math.ceil(self.numero_individuos * 0.2)
+        # primero seleccionamos los individuos que van a pasar sin cruces, es decir, pasan tal cual:
+        # seleccionamos el 20% aleatoriamente
+        numero= math.ceil(self.numero_individuos * self.probabilidad_no_cruce)
         indices_aleatorios = random.sample(range(0, self.padres.shape[0]), numero)
 
-        #Los añadimos en las n primeras filas de hijos
+        # los añadimos en las n primeras filas de hijos
         seleccionados = self.padres[indices_aleatorios]
         hijos[:numero] = seleccionados
         
-        #Para el resto de individuos cruzamos aleatoriamente dos de los padres
-        while numero < self.numero_individuos-1:
+        # para el resto de individuos cruzamos aleatoriamente dos de los padres
+        while numero < self.numero_individuos:
 
-            #Seleccionamos dos padres aletoriamente
+            # seleccionamos dos padres aletoriamente
             indices = random.sample(range(0, self.padres.shape[0]), 2)
             selec_cruce = self.padres[indices]
 
-            #Seleccionamos por qué parte de la lista vamos a partir aleatoriamente
+            # seleccionamos por qué parte de la lista vamos a partir aleatoriamente
             num = random.randint(0, self.padres.shape[1]-1)
 
-            #Seleccionamos los n primeros elementos del 1
+            # seleccionamos los n primeros elementos del 1
             padre1 = selec_cruce[0][:num]
 
             # y los siguientes del segundo
             padre2 = selec_cruce[1][num:]
 
-            # Crear un nuevo array combinando los elementos seleccionados
+            # crear un nuevo array combinando los elementos seleccionados
             hijo = np.concatenate((padre1, padre2))
 
-            numero+=1
             hijos[numero] = hijo
+            numero+=1
 
         return hijos
 
-
-
-    
