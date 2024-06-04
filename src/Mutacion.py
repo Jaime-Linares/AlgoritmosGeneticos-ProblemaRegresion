657ece8d8ee093eb0eb7c9835da011cb63f893ec
Jaime-Linares <jailinbar@alum.us.es>
2024-06-04 13:24:29 +0200

    [fix] corrections in mutation process

--- src/Mutacion.py
+++ src/Mutacion.py
@@ -20,14 +20,13 @@ class Mutacion:
         # mutamos los individuos seleccionados
         for i in indices_aleatorios:
             gen = random.randint(0, self.hijos.shape[1]-1)      # elegimos el gen que mutará
-            if gen in range(0, 2 * self.hijos.shape[1], 2) or gen == 2 * self.hijos.shape[1]:     # si es una constante o el término independiente
-                if self.hijos[i, gen] >= 50:                            # si el gen es mayor o igual a 50 le restamos 50
-                    hijos_mutados[i, gen] = self.hijos[i, gen] - 50
-                elif self.hijos[i, gen] <= -50:                         # si el gen es menor o igual a -50 le sumamos 50
-                    hijos_mutados[i, gen] = self.hijos[i, gen] + 50
-                else:                                                   # si el gen está entre -50 y 50 lo dividimos por 2
-                    hijos_mutados[i, gen] = self.hijos[i, gen] / 2
-            elif gen in range(1, 2 * self.hijos.shape[1], 2):       # si es posición impar (exponente) excepto el término independiente
+
+            # si es una constante o el término independiente, cambiamos el valor por otro valor random entre -100 y 100
+            if gen in range(0, 2 * self.hijos.shape[1], 2) or gen == 2 * self.hijos.shape[1]:  
+                hijos_mutados[i, gen] = (hijos_mutados[i,gen] + np.random.uniform(-100, 100))/2
+
+            # si es posición impar (exponente) excepto el término independiente
+            elif gen in range(1, 2 * self.hijos.shape[1], 2):    
                 if self.hijos[i, gen] >-1 and self.hijos[i, gen] <5:    # si el exponente está entre -1 y 5, aleatoriamente se suma o resta 1
                     numero_aleatorio = random.randint(0, 1)
                     if(numero_aleatorio==0):
@@ -38,6 +37,7 @@ class Mutacion:
                     hijos_mutados[i, gen] += 1
                 else:                                                   # si el exponentes 5, se le resta 1
                     hijos_mutados[i, gen] -= 1 
+
         return hijos_mutados
     
 
