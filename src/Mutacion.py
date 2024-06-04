4032f6ae766f716651ab01b4059ae55b335a511d
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-06-04 17:21:32 +0200

    [fix] improve mutation method (multiple gens)

--- src/Mutacion.py
+++ src/Mutacion.py
@@ -16,27 +16,35 @@ class Mutacion:
         # elegimos el numero de individuos que mutarán y cuales van a ser
         numero_mutaciones = math.ceil(self.probabilidad * self.hijos.shape[0])
         indices_aleatorios = random.sample(range(0, self.hijos.shape[0]), numero_mutaciones)
+        
+        #Para cada individuo se mutará un número de genes aleatorios que van desde 1 hasta el 10% del total de genes de individuo
+        
+        
 
         # mutamos los individuos seleccionados
         for i in indices_aleatorios:
-            gen = random.randint(0, self.hijos.shape[1]-1)      # elegimos el gen que mutará
+            num_genes_mutar= random.randint(1, math.ceil(self.hijos.shape[1]*0.1))
+            j=0
+            while j <= num_genes_mutar:
+                j+=1
+                gen = random.randint(0, self.hijos.shape[1]-1)      # elegimos el gen que mutará
 
-            # si es una constante o el término independiente, cambiamos el valor por otro valor random entre -100 y 100
-            if gen in range(0, 2 * self.hijos.shape[1], 2) or gen == 2 * self.hijos.shape[1]:  
-                hijos_mutados[i, gen] = (hijos_mutados[i,gen] + np.random.uniform(-100, 100))/2
+                # si es una constante o el término independiente, cambiamos el valor por otro valor random entre -100 y 100
+                if gen in range(0, 2 * self.hijos.shape[1], 2) or gen == 2 * self.hijos.shape[1]:  
+                    hijos_mutados[i, gen] = (hijos_mutados[i,gen] + np.random.uniform(-50, 50))/2
 
-            # si es posición impar (exponente) excepto el término independiente
-            elif gen in range(1, 2 * self.hijos.shape[1], 2):    
-                if self.hijos[i, gen] >-1 and self.hijos[i, gen] <5:    # si el exponente está entre -1 y 5, aleatoriamente se suma o resta 1
-                    numero_aleatorio = random.randint(0, 1)
-                    if(numero_aleatorio==0):
+                # si es posición impar (exponente) excepto el término independiente
+                elif gen in range(1, 2 * self.hijos.shape[1], 2):    
+                    if self.hijos[i, gen] >-1 and self.hijos[i, gen] <5:    # si el exponente está entre -1 y 5, aleatoriamente se suma o resta 1
+                        numero_aleatorio = random.randint(0, 1)
+                        if(numero_aleatorio==0):
+                            hijos_mutados[i, gen] += 1
+                        else:
+                            hijos_mutados[i, gen] -= 1
+                    elif self.hijos[i, gen] == -1:                          # si el exponente es -1, se le suma 1
                         hijos_mutados[i, gen] += 1
-                    else:
-                        hijos_mutados[i, gen] -= 1
-                elif self.hijos[i, gen] == -1:                          # si el exponente es -1, se le suma 1
-                    hijos_mutados[i, gen] += 1
-                else:                                                   # si el exponentes 5, se le resta 1
-                    hijos_mutados[i, gen] -= 1 
+                    else:                                                   # si el exponentes 5, se le resta 1
+                        hijos_mutados[i, gen] -= 1 
 
         return hijos_mutados
     
